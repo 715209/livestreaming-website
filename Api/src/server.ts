4fce68ef3ad7cbff60111c402803ac4d90fa48a4
Brian Spit <brian@mythenatic.com>
2018-12-05 20:55:34 +0100

    Remove /api/

--- Api/src/server.ts
+++ Api/src/server.ts
@@ -7,40 +7,43 @@ import * as compression from "compression";
 import * as cors from "cors";
 
 // Import routers
-import userRouter from './routers/userRouter';
-import publishRouter from './routers/publishRouter';
+import userRouter from "./routers/userRouter";
+import publishRouter from "./routers/publishRouter";
 // Server class
 class Server {
-    public app: express.Application;
+  public app: express.Application;
 
-    constructor() {
-        this.app = express();
-        this.config();
-        this.routes();
-    }
+  constructor() {
+    this.app = express();
+    this.config();
+    this.routes();
+  }
 
-    public config() {
-        const MONGO_URI = "mongodb://localhost/project44";
+  public config() {
+    const MONGO_URI = "mongodb://localhost/project44";
 
-        mongoose.connect(MONGO_URI || process.env.MONGODB_URI, { useNewUrlParser: true });
+    mongoose.connect(
+      MONGO_URI || process.env.MONGODB_URI,
+      { useNewUrlParser: true }
+    );
 
-        this.app.use(bodyParser.urlencoded({ extended: true }));
-        this.app.use(bodyParser.json());
-        this.app.use(helmet());
-        this.app.use(logger("dev"));
-        this.app.use(compression());
-        this.app.use(cors());
-    }
+    this.app.use(bodyParser.urlencoded({ extended: true }));
+    this.app.use(bodyParser.json());
+    this.app.use(helmet());
+    this.app.use(logger("dev"));
+    this.app.use(compression());
+    this.app.use(cors());
+  }
 
-    public routes(): void {
-        let router: express.Router;
+  public routes(): void {
+    let router: express.Router;
 
-        router = express.Router();
+    router = express.Router();
 
-        this.app.use("/", router);
-        this.app.use("/api/v1/publish", publishRouter);
-        this.app.use("/api/v1/users", userRouter);
-    }
+    this.app.use("/", router);
+    this.app.use("/v1/publish", publishRouter);
+    this.app.use("/v1/users", userRouter);
+  }
 }
 
 export default new Server().app;
