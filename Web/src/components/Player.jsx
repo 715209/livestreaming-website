b41c625d9744d8e06896628d40beb216dab6afa3
Brian Spit <brian@mythenatic.com>
2018-12-08 01:46:07 +0100

    fixed IOS video and removed auto create/delete video will need to look at it later

--- Web/src/components/Player.jsx
+++ Web/src/components/Player.jsx
@@ -10,16 +10,19 @@ const PlayerStyle = styled.div`
 
 const VideoStyle = styled.video`
   width: 100%;
-  height: 100%;
-  background: #000000;
+
+  @media (min-width: 800px) {
+    height: 100%;
+  }
 `;
 
 class Player extends Component {
   createHlsStream() {
+    const video = this.videoPlayer;
+    const url = `localhost:3002/hls/${this.props.data.username}.m3u8`;
+
     if (Hls.isSupported() && this.videoPlayer && this.props.data.channel.live) {
       this.hls = new Hls({ liveDurationInfinity: true });
-      const video = this.videoPlayer;
-      const url = `localhost:3002/hls/${this.props.data.username}.m3u8`;
 
       this.hls.loadSource(url);
       this.hls.attachMedia(video);
@@ -34,7 +37,29 @@ class Player extends Component {
             })
             .catch(error => {
               // Autoplay was prevented.
-              // Show a "Play" button so that user can start playback.
+              console.log("Autoplay prevented let's play the video muted!");
+              video.muted = true;
+              video.controls = true;
+              video.play();
+            });
+        }
+      });
+    } else if (
+      video.canPlayType("application/vnd.apple.mpegurl") &&
+      this.props.data.channel.live
+    ) {
+      video.src = url;
+      video.addEventListener("loadedmetadata", () => {
+        const promise = video.play();
+
+        if (promise !== undefined) {
+          promise
+            .then(() => {
+              // Autoplay started!
+              console.log("Autoplay started!");
+            })
+            .catch(error => {
+              // Autoplay was prevented.
               console.log("Autoplay prevented let's play the video muted!");
               video.muted = true;
               video.controls = true;
@@ -57,15 +82,14 @@ class Player extends Component {
   }
 
   componentDidUpdate(prevProps) {
-    if (!this.props.data.channel.live && this.hls) {
-      this.hls.destroy();
-      console.log("destroyed hls");
-    }
-
-    if (this.props.data.channel.live && !this.hls) {
-      this.createHlsStream();
-      console.log("created hls");
-    }
+    // if (!this.props.data.channel.live && this.hls) {
+    //   this.hls.destroy();
+    //   console.log("destroyed hls");
+    // }
+    // if (this.props.data.channel.live && !this.hls) {
+    //   this.createHlsStream();
+    //   console.log("created hls");
+    // }
   }
 
   render() {
@@ -77,9 +101,9 @@ class Player extends Component {
           playsInline={true}
           poster="https://via.placeholder.com/1920x1080?text=Livestream+is+currently+offline"
         />
-        <div className="control">
+        {/* <div className="control">
           <h1>Yes hello</h1>
-        </div>
+        </div> */}
       </PlayerStyle>
     );
   }
